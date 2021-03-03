ccbd794d04bc2f6e8cc5bf02dd29ce5212df7eaf
Giulio Zanchetta <zank@outlook.it>
2021-03-03 09:11:49 +0100

    Create backend project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9f2f2e2808935875481c367d35753484'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '99d8bc84-56cd-4749-aeb9-cc4f472d0686'
+};
